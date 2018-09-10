2aa7562126b8535ee4623a246adc1af534b751b4
James <jamesaldrin.reyes.iics@ust.edu.ph>
2018-09-10 10:14:24 +0800

    V1

--- app/src/main/java/jamesaldrin/reyes/com/reyesjamesaldrinlab1/RelativeLayout.java
+++ app/src/main/java/jamesaldrin/reyes/com/reyesjamesaldrinlab1/RelativeLayout.java
@@ -8,6 +8,6 @@ public class RelativeLayout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_layout);
+        setContentView(R.layout.constraint_layout);
     }
 }
